ac3219887f5963956ea1d740f4288dd9b71fa185
sofgqv <sofia.garcia@utec.edu.pe>
2023-05-03 20:44:09 -0500

    test seq

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,59 +1,35 @@
-#include "../AVL.h"
-#include "../Sequential.h"
-#include "../ExtendibleHash.h"
-#include <chrono>
 #include <Entities/Payment.h>
 #include <Entities/Product.h>
 
-string dataFilePath = "datafile.dat";
-string auxFilePath = "auxfile.dat";
-string indexFilePath = "indexFile.dat";
-string bucketFilePath = "bucketFile.dat";
 
-template<typename Record>
-void AVLTest(vector<Record>& data){
-    cout << "----------------------------------------" << endl;
-    cout << "Start Test " << data.size() <<" - AVL" << endl;
-    AVLFile avl(dataFilePath);
-    vector<Record> vr = data;
+vector<Payment> readPayments(string filename){
+    vector<Payment> payments;
+    ifstream archivo(filename);
+    string linea;
 
-    // Insert
-    auto start  = chrono::steady_clock::now();
-    for(int i = 0; i < vr.size(); i++){
-        avl.insert(vr[i]);
+    if(!archivo.is_open()){
+        cout << "No se pudo abrir el archivo." << endl;
     }
-    auto end    = chrono::steady_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
 
-    // Search
-    start  = chrono::steady_clock::now();
-    const char* codigo = vr[100].getPrimaryKey();
-    avl.search(codigo);
-    end    = chrono::steady_clock::now();
-    duration = chrono::duration_cast<chrono::microseconds>(end-start).count();
-    cout <<"Search duration: " <<duration << endl;
-    cout << "Finish Test "<< data.size() <<" - Hash" << endl;
-    cout << "----------------------------------------" << endl;
-}
+    while (getline(archivo, linea)) {
+        istringstream ss(linea);
+        string order_id, payment_type;
+        int payment_sequential, payment_installments;
+        float payment_value;
+        char cstr[32], ctr[11];
 
-template <typename Record=Payment>
-vector<Record> getData(string& path, int amount){
+        getline(ss, order_id, ',');
+        std::strcpy(cstr, order_id.c_str());
+        ss >> payment_sequential;
+        ss.ignore();
+        getline(ss, payment_type, ',');
+        std::strcpy(ctr, payment_type.c_str());
+        ss >> payment_installments;
+        ss.ignore();
+        ss >> payment_value;
 
-}
-
-template <typename Record=Payment>
-void PARSERTEST(){
-    string filename = "olist_order_payments_dataset.csv";
-    vector<int> amount = {10000, 50000, 100000};
-    for(int i = 0; i < amount.size(); i++){
-        cout <<"-------------------------------------------" << endl;
-        vector<Record> data = getData(filename, amount[i]);
-        AVLTest(data);
+        Payment pago(cstr, payment_sequential, ctr, payment_installments, payment_value);
+        payments.push_back(pago);
     }
+    return payments;
 }
-
-int main(){
-    PARSERTEST();
-    return 0;
-}
-
