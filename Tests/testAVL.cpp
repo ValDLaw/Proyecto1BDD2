01309aaccdab32bf373bfbfe598ebb99eab93363
sofgqv <sofia.garcia@utec.edu.pe>
2023-05-04 03:19:47 -0500

    avl test

--- Tests/testAVL.cpp
+++ Tests/testAVL.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
-#include "../Entities/Product.h"
 #include "../Entities/Payment.h"
 #include "../AVL.h"
 #include "../Tests/tests.cpp"
@@ -10,53 +8,31 @@
 
 using namespace	std;
 
+template<typename Record=IntPayment>
+void paymentTest(vector<Record> data, int size){
+    cout << "----------------------------------------" << endl;
+    cout << "START TEST " << size << endl;
+    AVL<Payment> Arbolito("TestPayment" + to_string(size) + ".dat");
 
-
-void TestPayment(){
-    AVL<Payment> Arbolito10("TestPayment10.dat");
-    string direc = "/Users/ValDLaw/Documents/GitHub/Proyecto1BDD2/Datasets/olist_order_payments_dataset.csv";
-    cout << "----------------------------AVL TEST---------------------------" << endl;
-
-    auto data = readPayments(direc);
-    cout << "\nINICIANDO TEST CON 10K" << endl;
-    cout << "************************" << endl;
+    // INSERT
     int count = 0;
     auto start  = chrono::steady_clock::now();
-    for(auto i = 0; i<= data.size()/10; i++) {
+    for(int i = 0; i < size; i++){
         count++;
-        Arbolito10.insert(data[i]);
+        Arbolito.insert(data[i]);
     }
     auto end = chrono::steady_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-    cout << count <<" Records - Insert Duration in miliseconds: " <<duration << endl;
-    cout << "Number of Memory Accesses:  " << Arbolito10.getNumberAccess() << endl;
+    cout << "INSERT: " << duration << "milisegundos" << endl;
 
-    cout << "\nINICIANDO TEST CON 50K" << endl;
-    cout << "************************" << endl;
-    count = 0;
-    AVL<Payment> Arbolito50("TestPayment.dat");
+    // SEARCH
     start  = chrono::steady_clock::now();
-    for(auto i = 0; i<= data.size()/2; i++) {
-        count++;
-        Arbolito50.insert(data[i]);
-    }
+    vector<Payment> vec = Arbolito.search((char*)"8a0ecc540156c0f21f25d7bfd4e95dbe");
     end = chrono::steady_clock::now();
-    duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-    cout << count <<" Records - Insert Duration in miliseconds: " <<duration << endl;
-    cout << "Number of Memory Accesses:  " << Arbolito50.getNumberAccess() << endl;
+    duration = chrono::duration_cast<chrono::nanoseconds>(end-start).count();
+    cout <<"SEARCH: " << duration << "nanosegundos" << endl;
 
-    cout << "\nINICIANDO TEST CON 100K" << endl;
-    cout << "************************" << endl;
-    count = 0;
-    AVL<Payment> Arbolito100("TestPayment.dat");
-    start  = chrono::steady_clock::now();
-    for(auto i = 0; i<= data.size(); i++) {
-        count++;
-        Arbolito100.insert(data[i]);
-    }
-    end = chrono::steady_clock::now();
-    duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-    cout << count <<" Records - Insert Duration in miliseconds: " <<duration << endl;
-    cout << "Number of Memory Accesses:  " << Arbolito100.getNumberAccess() << endl;
-
+    cout << "SECONDARY ACCESS" << Arbolito.getNumberAccess() << endl;
+    cout << "FINISH TEST "<< size << endl;
+    cout << "----------------------------------------" << endl;
 }
--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,6 +1,38 @@
 #include "Entities/IntPayment.h"
 #include <vector>
 
+vector<Payment> readPayments(string filename){
+    vector<Payment> payments;
+    ifstream archivo(filename);
+    string linea;
+
+    if(!archivo.is_open()){
+        cout << "No se pudo abrir el archivo." << endl;
+    }
+
+    while (getline(archivo, linea)) {
+        istringstream ss(linea);
+        string order_id, payment_type;
+        int payment_sequential, payment_installments;
+        float payment_value;
+        char cstr[32], ctr[11];
+
+        getline(ss, order_id, ',');
+        std::strcpy(cstr, order_id.c_str());
+        ss >> payment_sequential;
+        ss.ignore();
+        getline(ss, payment_type, ',');
+        std::strcpy(ctr, payment_type.c_str());
+        ss >> payment_installments;
+        ss.ignore();
+        ss >> payment_value;
+
+        Payment pago(cstr, payment_sequential, ctr, payment_installments, payment_value);
+        payments.push_back(pago);
+    }
+    return payments;
+}
+
 vector<IntPayment> readIntPayments(string filename){
     vector<IntPayment> payments;
     ifstream archivo(filename);
