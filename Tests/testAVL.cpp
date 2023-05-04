e11e95004f3d7bc3cfb992812139a9ad8307d216
Enzo <enzo.camizan@utec.edu.pe>
2023-05-03 21:10:20 -0500

    Update Test AVL

--- Tests/testAVL.cpp
+++ Tests/testAVL.cpp
@@ -51,22 +51,22 @@ void TestProduct(){
 }
 
 void TestPayment(){
-  AVL<Payment> Arbolito("TestPayment.dat");
-    string direc = "/Users/ValDLaw/Documents/GitHub/Proyecto1BDD2/Datasets/olist_order_payments_dataset.csv";
+    AVL<Payment> Arbolito10("TestPayment10.dat");
+    string direc = "/Users/ValDLaw/Documents/GitHub/Proyecto1BDD2/Datasets/olist_products_dataset.csv";
     ifstream archivo(direc);
     string linea;
     cout << "----------------------------AVL TEST---------------------------" << endl;
 
     if(!archivo.is_open()){
         cout << "No abrio" << endl;
-        return;
+
     }
 
     cout << "\nINICIANDO TEST CON 10K" << endl;
     cout << "************************" << endl;
     int count = 0;
     auto start  = chrono::steady_clock::now();
-    while (getline(archivo, linea) and count != 10001) {
+    while (getline(archivo, linea) && count != 10001) {
         count++;
         istringstream ss(linea);
         string order_id, payment_type;
@@ -85,17 +85,22 @@ void TestPayment(){
         ss >> payment_value;
 
         Payment pago(cstr, payment_sequential, ctr, payment_installments, payment_value);
-        Arbolito.insert(pago);
+        Arbolito10.insert(pago);
     }
     auto end = chrono::steady_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
     cout << count <<" Records - Insert Duration: " <<duration << endl;
+    cout << "Number of Memory Accesses:  " <<Arbolito10.getNumberAccess() << endl;
+
+
+    AVL<Payment> Arbolito50("TestPayment50.dat");
 
     cout << "\nINICIANDO TEST CON 50K" << endl;
     cout << "************************" << endl;
     count = 0;
     start  = chrono::steady_clock::now();
-    while (getline(archivo, linea) and count != 50001) {
+
+    while (getline(archivo, linea) && count != 50001) {
         count++;
         istringstream ss(linea);
         string order_id, payment_type;
@@ -114,12 +119,14 @@ void TestPayment(){
         ss >> payment_value;
 
         Payment pago(cstr, payment_sequential, ctr, payment_installments, payment_value);
-        Arbolito.insert(pago);
+        Arbolito50.insert(pago);
     }
     end = chrono::steady_clock::now();
     duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
     cout << count <<" Records - Insert Duration: " <<duration << endl;
+    cout << "Number of Memory Accesses:  " <<Arbolito50.getNumberAccess() << endl;
 
+    AVL<Payment> Arbolito100("TestPayment100.dat");
     cout << "\nINICIANDO TEST CON 100K" << endl;
     cout << "************************" << endl;
     count = 0;
@@ -143,11 +150,13 @@ void TestPayment(){
         ss >> payment_value;
 
         Payment pago(cstr, payment_sequential, ctr, payment_installments, payment_value);
-        Arbolito.insert(pago);
+        Arbolito100.insert(pago);
     }
     end = chrono::steady_clock::now();
     duration = chrono::duration_cast<chrono::milliseconds>(end-start).count();
     cout << count <<" Records - Insert Duration: " <<duration << endl;
+    cout << "Number of Memory Accesses:  " <<Arbolito100.getNumberAccess() << endl;
 
     archivo.close();
+
 }
