2207582e7854a042496ac5a9c4163942d8c76a93
sofgqv <sofia.garcia@utec.edu.pe>
2023-05-09 10:34:29 -0500

    demostration sequential

--- Tests/testSequential.cpp
+++ Tests/testSequential.cpp
@@ -2,6 +2,47 @@
 #include <chrono>
 #include "tests.cpp"
 
+void demostrationSequential(){
+    string data = "../SQ_data.dat";
+    string aux = "../SQ_aux.dat";
+
+    SequentialFile<IntPayment> seqFile(data, aux);
+    cout << "INSERT" << endl;
+    seqFile.add(IntPayment(3,1,"credit_card",6,90.99));
+    seqFile.add(IntPayment(9,1,"voucher",1,40.99));
+    seqFile.add(IntPayment(4,1,"credit_card",2,99.00));
+    seqFile.add(IntPayment(5,1,"boleto",4,19.00));
+    seqFile.add(IntPayment(6,1,"credit_card",1,32.99));
+    seqFile.add(IntPayment(7,1,"boleto",5,72.99));
+    seqFile.add(IntPayment(8,1,"voucher",2,16.20));
+    seqFile.add(IntPayment(12,1,"credit_card",2,10.99));
+    seqFile.add(IntPayment(13,1,"boleto",1,15.99));
+    seqFile.add(IntPayment(14,1,"credit_card",2,20.00));
+    seqFile.read();
+
+    cout << "SEARCH 4" << endl;
+    auto find5 = seqFile.search(4);
+    if (find5.empty()){cout << "WRONG: 4 not found" << endl;}
+    else{cout << "CORRECT: 4 found" << endl;}
+    cout << "SEARCH 10" << endl;
+    find5 = seqFile.search(10);
+    if (find5.empty()){cout << "CORRECT: 10 not found" << endl;}
+    else{cout << "WRONG: 10 found" << endl;}
+    auto getvalues = seqFile.rangeSearch(2,10);
+    cout << "SEARCH FROM 2 TO 10" << endl;
+    for (auto item : getvalues){
+        cout << item.getPrimaryKey() << ",";
+    }
+    cout << endl;
+
+    for (auto i=110; i!=0; i--){
+        seqFile.add(IntPayment(i, 2, "boleto", 1, 99.99));
+    }
+    cout << "REBUILD" << endl;
+    seqFile.read();
+    cout << endl;
+};
+
 template<typename Record=IntPayment>
 void paymentTest(vector<Record> data, int size){
     cout << "----------------------------------------" << endl;
--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "Tests/tests.cpp"
 #include "Tests/testAVL.cpp"
+#include "Tests/testSequential.cpp"
 
 using namespace std;
 
 int main(){
-    auto data = readPayments("/Users/ValDLaw/Documents/GitHub/Proyecto1BDD2/Datasets/olist_order_payments_dataset.csv");
-    paymentTest(data,10000);
-    paymentTest(data,50000);
-    paymentTest(data,100000);
+    demostrationSequential();
 };
